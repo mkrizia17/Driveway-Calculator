79703fe544e43009e3b6228a31aae5f98aef0e59
kurija17 <maria17workemail@gmail.com>
2024-11-28 18:15:24 +0800

    Add menu nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,40 @@
 import Link from 'next/link';
-import { CalculatorIcon } from 'lucide-react';
+import { HomeIcon } from 'lucide-react';
 
 export function Header() {
   return (
     <div className="bg-gray-900 border-b border-gray-700 h-20 flex items-center" style={{ fontFamily: '__Inter_d65c78, __Inter_Fallback_d65c78, sans-serif' }}>
-      <div className="container mx-auto px-4">
+      <div className="container mx-auto px-4 flex items-center justify-center">
         <Link 
           href="/" 
-          className="text-gray-100 hover:text-blue-400 transition-colors flex items-center gap-3 text-3xl font-bold"
+          className="text-gray-100 hover:text-blue-400 transition-colors flex items-center gap-3 text-1xl mx-4 uppercase"
         >
-          <CalculatorIcon className="w-8 h-8" />
-          Driveway Calculator
+          <HomeIcon className="w-8 h-8" />
+          HOME
+        </Link>
+        <Link 
+          href="/gravel-calculator" 
+          className="text-gray-100 hover:text-blue-400 transition-colors text-1xl mx-8 uppercase"
+        >
+          GRAVEL
+        </Link>
+        <Link 
+          href="/concrete-calculator" 
+          className="text-gray-100 hover:text-blue-400 transition-colors text-1xl mx-8 uppercase"
+        >
+          CONCRETE 
+        </Link>
+        <Link 
+          href="/asphalt-calculator" 
+          className="text-gray-100 hover:text-blue-400 transition-colors text-1xl mx-8 uppercase"
+        >
+          ASPHALT 
+        </Link>
+        <Link 
+          href="/paver-calculator" 
+          className="text-gray-100 hover:text-blue-400 transition-colors text-1xl mx-8 uppercase"
+        >
+          PAVER 
         </Link>
       </div>
     </div>
